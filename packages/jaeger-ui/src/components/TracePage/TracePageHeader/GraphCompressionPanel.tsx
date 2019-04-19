08bb4f17e5c265c0fcd669d98ceea71b1068b0d4
Everett Ross <reverett@uber.com>
2019-04-19 18:35:08 -0400

    WIP - Clean up HTML, Begin Popover
    Signed-off-by: Everett Ross <reverett@uber.com>

--- packages/jaeger-ui/src/components/TracePage/TracePageHeader/GraphCompressionPanel.tsx
+++ packages/jaeger-ui/src/components/TracePage/TracePageHeader/GraphCompressionPanel.tsx
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 import * as React from 'react';
-import { Button, Checkbox, Modal, Switch } from 'antd';
+import { Button, Checkbox, Modal, Popover, Switch } from 'antd';
 import cx from 'classnames';
 
 import IoIosInformationCircle from 'react-icons/lib/io/informatcircled';
@@ -25,7 +25,14 @@ import EviDdg from '../../../img/evi-ddg.svg';
 
 import './GraphCompressionPanel.css';
 
-type detailLevel = 'off' | 'UvD' | 'UƒP' | 'EvI';
+type detailLevel = 'off' | 'UvD' | 'PPE' | 'EvI';
+
+const detailLevelSvgMap: Record<detailLevel, string> = {
+  off: ConciseDdg,
+  UvD: UvdDdg,
+  PPE: PpeDdg,
+  EvI: EviDdg,
+};
 
 export default class GraphCompressionPanel extends React.PureComponent<
   {},
@@ -42,6 +49,23 @@ export default class GraphCompressionPanel extends React.PureComponent<
   closeModal = () => {
     this.setState({ detailModalOpen: false });
   };
+
+  getTargetLevel = (thisLevel: detailLevel): detailLevel => {
+    const { detailLevel: stateLevel } = this.state;
+    if (stateLevel !== thisLevel) return thisLevel;
+
+    switch(thisLevel) {
+      case('EvI'): {
+        return 'PPE';
+      }
+      case('PPE'): {
+        return 'UvD';
+      }
+      default: {
+        return 'off';
+      }
+    }
+  }
 
   toggleSvSO = () => {
     this.setState({ operation: !this.state.operation });
@@ -65,23 +89,6 @@ export default class GraphCompressionPanel extends React.PureComponent<
           <Switch checked={operation} onChange={this.toggleSvSO} size="small" />
           <span>Service and Operation</span>
         </Button>
-        <div style={{ justifySelf: 'center', margin: '0 20px' }}>
-          <span>Hops:</span>
-          <Button
-            disabled={this.state.hops <= 1}
-            icon="minus"
-            htmlType="button"
-            onClick={() => this.setState({ hops: this.state.hops - 1 })}
-          />
-          {this.state.hops}
-          <Button
-            icon="plus"
-            htmlType="button"
-            onClick={() => this.setState({ hops: this.state.hops + 1 })}
-          />
-        </div>
-        <div style={{ justifySelf: 'flex-end' }}>
-        </div>
            */
         
   render() {
@@ -95,76 +102,34 @@ export default class GraphCompressionPanel extends React.PureComponent<
           <div className='expand-o-matic GraphDetail'>
             <span>
               Graph Detail <Button className="ant-btn-icon-only" htmlType="button" onClick={this.openModal} ><IoIosInformationCircle /></Button>:
-              <Modal align="center" closable title="Graph Detail Options" onCancel={this.closeModal} visible={this.state.detailModalOpen}>
-      {/*
-                <table>
-                  <tbody>
-                    <tr>
-                      <td>
-                        Make this my header Make this my header
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <img className="ddg-example" src={ConciseDdg}/>
-                      </td>
-                      <td>tables are lit</td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <img className="ddg-example" src={UvdDdg}/>
-                      </td>
-                      <td>tables are lit</td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <img className="ddg-example" src={PpeDdg}/>
-                      </td>
-                      <td>tables are life</td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <img className="ddg-example" src={EviDdg}/>
-                      </td>
-                      <td>tables are lit</td>
-                    </tr>
-                  </tbody>
-                </table>
-        */ }
+                <Modal align="center" closable title="Graph Detail Options" onCancel={this.closeModal} visible={this.state.detailModalOpen}>
                   <div>
-                    <h3 className="descriptor">
-                        Most Concise
-                    </h3>
+                    <h3 className="descriptor">Most Concise</h3>
                     <div className="img-row">
-                      <img className="ddg-example" src={ConciseDdg}/>
+                      <img className="ddg-example" src={detailLevelSvgMap['off']}/>
                       <p>Densest view of a Deep Dependency Graph, every service or service and operation will appear once.</p>
                     </div>
-                    <h3 className="descriptor">
-                        +Upstream v Downstream
-                    </h3>
+                    <h3 className="descriptor">+Upstream v Downstream</h3>
                     <div className="img-row">
-                      <img className="ddg-example" src={UvdDdg}/>
+                      <img className="ddg-example" src={detailLevelSvgMap['UvD']}/>
                       <p>More detailed view that distinguishes between upstream and downstream of the focal node. Previously it was ambiguous if the green node was only downstream of focal node, now it is clear.</p>
                     </div>
-                    <h3 className="descriptor">
-                        +Prevent Path Entanglement
-                    </h3>
+                    <h3 className="descriptor">+Prevent Path Entanglement</h3>
                     <div className="img-row">
-                      <img className="ddg-example" src={PpeDdg}/>
-      <div>
-      <p>By preventing the convergence of the downstream blue node into a single node, it is clear that the downstream blue node is only dependent on the yellow node when it is after the red node.</p>
-      <p>Additionally, this ensures both sides of the focal nodes are trees and that there are no cycles in the Deep Dependency Graph.
-      </p>
-      </div>
+                      <img className="ddg-example" src={detailLevelSvgMap['PPE']}/>
+                        <div>
+                          <p>By preventing the convergence of the downstream blue node into a single node, it is clear that the downstream blue node is only dependent on the yellow node when it is after the red node.</p>
+                          <p>Additionally, this ensures both sides of the focal nodes are trees and that there are no cycles in the Deep Dependency Graph.
+                          </p>
+                        </div>
                     </div>
-                    <h3 className="descriptor">
-                        +External v Internal
-                    </h3>
+                    <h3 className="descriptor">+External v Internal</h3>
                     <div className="img-row">
-                        <img className="ddg-example" src={EviDdg}/>
-      <div>
-      <p>Differentiating between External vertices (sources or leaves) and Internal vertices can help identify entry and exit points.</p><p>This can be used to ensure incoming requests are conditioned by a gateway service or that all interactions with a downstream service are persisted in a database.</p>
-      </div>
+                      <img className="ddg-example" src={detailLevelSvgMap['EvI']}/>
+                      <div>
+                        <p>Differentiating between External vertices (sources or leaves) and Internal vertices can help identify entry and exit points.</p>
+                        <p>This can be used to ensure incoming requests are conditioned by a gateway service or that all interactions with a downstream service are persisted in a database.</p>
+                      </div>
                     </div>
                   </div>
               </Modal>
@@ -172,37 +137,39 @@ export default class GraphCompressionPanel extends React.PureComponent<
             <label>
               <Checkbox
                 className="detail-check-bok"
-                onChange={({ target: { checked } }) =>
-                this.setState({
-                detailLevel: (checked || this.state.detailLevel !== 'UvD' ? 'UvD' : 'off') as detailLevel,
-                })
+                onChange={() =>
+                  this.setState({ detailLevel: this.getTargetLevel('UvD') })
                 }
-                checked={detailLevel === 'UvD' || detailLevel === 'UƒP' || detailLevel === 'EvI'}
+                checked={detailLevel === 'UvD' || detailLevel === 'PPE' || detailLevel === 'EvI'}
               />
-                  Upstream v Downstream
-              </label>
+              Upstream v Downstream
+            </label>
+            <label>
+              <Checkbox
+                className="detail-check-bok"
+                onChange={() =>
+                  this.setState({ detailLevel: this.getTargetLevel('PPE') })
+                }
+                checked={detailLevel === 'PPE' || detailLevel === 'EvI'}
+              />
+              Prevent Path Entanglement
+            </label>
+            <Popover
+              content={(
+                <div><img className="ddg-example" src={detailLevelSvgMap[this.state.detailLevel]} /><img className="ddg-example top" src={detailLevelSvgMap[this.getTargetLevel('EvI')]} /></div>
+                )}
+            >
               <label>
                 <Checkbox
                   className="detail-check-bok"
-                  onChange={({ target: { checked } }) =>
-                  this.setState({
-                  detailLevel: (checked || this.state.detailLevel !== 'UƒP' ? 'UƒP' : 'UvD') as detailLevel,
-                  })
+                  onChange={() =>
+                    this.setState({ detailLevel: this.getTargetLevel('EvI') })
                   }
-                  checked={detailLevel === 'UƒP' || detailLevel === 'EvI'}
+                  checked={detailLevel === 'EvI'}
                 />
-                    Prevent Path Entanglement
-                </label>
-                <label>
-                  <Checkbox
-                    className="detail-check-bok"
-                    onChange={({ target: { checked } }) =>
-                    this.setState({ detailLevel: (checked ? 'EvI' : 'UƒP') as detailLevel })
-                    }
-                    checked={detailLevel === 'EvI'}
-                  />
-                      External v Internal
-                  </label>
+                External v Internal
+              </label>
+            </Popover>
           </div>
         </div>
         <div>Hop Stuff</div>
